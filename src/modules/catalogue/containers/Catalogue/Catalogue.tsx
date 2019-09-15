f6dbed26ad851d9a9bee48291e5de29dfc4c9477
Arthur <arthur.robieux@gmail.com>
2019-09-15 16:58:04 +0200

    Fix POST

--- src/modules/catalogue/containers/Catalogue/Catalogue.tsx
+++ src/modules/catalogue/containers/Catalogue/Catalogue.tsx
@@ -1,12 +1,14 @@
 import React, { useContext, useState, useEffect } from "react";
 import { StoreContext } from "../../../../context/StoreContext";
 import { Title, Loader } from "../../../common-ui";
+import firebase from "firebase";
 
 import styles from "./styles.module.scss";
 
 export const Catalogue = () => {
   const store = useContext(StoreContext);
-  const [gateaux, setGateaux] = useState([] as string[]);
+  const [gateaux, setGateaux] = useState({} as any);
+  const [form, setForm] = useState({ name: "", description: "" });
 
   useEffect(() => {
     if (store.data && store.data.Gateaux) {
@@ -14,17 +16,38 @@ export const Catalogue = () => {
     }
   }, [store]);
 
+  const postFirebase = () => {
+    firebase
+      .database()
+      .ref("Gateaux")
+      .push({
+        name: form.name,
+        description: form.description
+      });
+  };
+
   if (!store.data) return <Loader />;
 
   return (
     <div>
       <Title>Catalogue</Title>
       <div className={styles.blocks}>Bienvenue dans le catalogue</div>
+      <input
+        type="text"
+        value={form.name}
+        onChange={evt => setForm({ ...form, name: evt.target.value })}
+      />
+      <input
+        type="text"
+        value={form.description}
+        onChange={evt => setForm({ ...form, description: evt.target.value })}
+      />
+      <button onClick={() => postFirebase()}>Ajouter</button>
       <div>
         {gateaux &&
-          gateaux.map((gateau: any) => (
+          Object.keys(gateaux).map((key: any) => (
             <div>
-              {gateau.name} {gateau.description}
+              {gateaux[key].name} {gateaux[key].description}
             </div>
           ))}
       </div>
