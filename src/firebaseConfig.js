a03c550ad50a734f4b9eebd1e488c1da05611a19
Arthur <arthur.robieux@gmail.com>
2020-01-15 12:38:25 +0100

    Use local firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,15 +2,28 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/storage";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyBKuSSYa6flZOe3zB0pLVJnaOxGxEVAvmw",
-  authDomain: "sugarqueen-cce31.firebaseapp.com",
-  databaseURL: "https://sugarqueen-cce31.firebaseio.com",
-  projectId: "sugarqueen-cce31",
-  storageBucket: "sugarqueen-cce31.appspot.com",
-  messagingSenderId: "1097346693497",
-  appId: "1:1097346693497:web:df8167963acf3c8b"
-};
+export const isLocal = window.location.hostname === "localhost";
+
+export const firebaseConfig = isLocal
+  ? {
+      apiKey: "AIzaSyDCEESiSInhy6yIdTBjul6psLvQOqDvhwg",
+      authDomain: "sugarqueen-dev.firebaseapp.com",
+      databaseURL: "https://sugarqueen-dev.firebaseio.com",
+      projectId: "sugarqueen-dev",
+      storageBucket: "sugarqueen-dev.appspot.com",
+      messagingSenderId: "540898077396",
+      appId: "1:540898077396:web:e03fa3c720d5bb1448a941",
+      measurementId: "G-V6M308GV0M"
+    }
+  : {
+      apiKey: "AIzaSyBKuSSYa6flZOe3zB0pLVJnaOxGxEVAvmw",
+      authDomain: "sugarqueen-cce31.firebaseapp.com",
+      databaseURL: "https://sugarqueen-cce31.firebaseio.com",
+      projectId: "sugarqueen-cce31",
+      storageBucket: "sugarqueen-cce31.appspot.com",
+      messagingSenderId: "1097346693497",
+      appId: "1:1097346693497:web:df8167963acf3c8b"
+    };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
 
